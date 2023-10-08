e39be250adbf2111055747064d00ca85a1b72b4e
evangkirkham <147008167+evangkirkham@users.noreply.github.com>
2023-10-08 14:46:25 -0700

    Image tracking
    This code will track an image on your screen for the color you specify. In this sample, I chose blue (255,0,0). It will only work for the darkest blues so more washed blue will not be detected, an easy fix by changing the color range. If no object of the color is within the frame, then no box will appear, if an object is presented of the specified color, a bounding box will appear around the object in frame.

--- videocapture.py
+++ videocapture.py
@@ -1,46 +1,53 @@
-import cv2
+# video capture with redbox and image tracking
+
+import cv2 as cv
 import numpy as np
-import matplotlib.pyplot as plt
-from sklearn.cluster import KMeans
+from PIL import Image
+
+def limits(color):
+    #insert BGR values to convert into HSV
+    c = np.uint8([[color]])
+    hsvC = cv.cvtColor(c, cv.COLOR_BGR2HSV)
+
+    # Give color range to find the upper and lower limits of the color you want to track
+    lowerLim = hsvC[0][0][0] - 10, 100, 100
+    upperLim = hsvC[0][0][0] + 10, 255, 255
+
+    lowerLim = np.array(lowerLim, dtype = np.uint8)
+    upperLim = np.array(upperLim, dtype = np.uint8)
+
+    return lowerLim, upperLim
+
+# initialize video capture
+cap = cv.VideoCapture(0) 
+
+# color we want to detect
+blue = [255, 0, 0]
+
+while True:
+    ret, frame = cap.read()
+
+    # adding color detection
+    hsvImg = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
+    lowerLimit, upperLimit = limits(color = blue)
+    mask = cv.inRange(hsvImg, lowerLimit, upperLimit)
+
+    # adding bounding box
+    newmask = Image.fromarray(mask) # converts frame to be black and highlight in white all objects in frame of color specified to detect
+    box = newmask.getbbox() # gets the box location of object
+
+    # print box to frame
+    if box is not None:
+        x1, y1, x2, y2 = box
+        frame = cv.rectangle(frame, (x1,y1), (x2,y2), (0,255,0), 5)
 
 
-def find_histogram(clt):
-    """
-    create a histogram with k clusters
-    :param: clt
-    :return:hist
-    """
-    numLabels = np.arange(0, len(np.unique(clt.labels_)) + 1)
-    (hist, _) = np.histogram(clt.labels_, bins=numLabels)
+    cv.imshow('frame', frame)
 
-    hist = hist.astype("float")
-    hist /= hist.sum()
+    # close program when 'q' is pressed
+    if cv.waitKey(1) & 0xff == ord('q'):
+        break
 
-    return hist
-def plot_colors2(hist, centroids):
-    bar = np.zeros((50, 300, 3), dtype="uint8")
-    startX = 0
 
-    for (percent, color) in zip(hist, centroids):
-        # plot the relative percentage of each cluster
-        endX = startX + (percent * 300)
-        cv2.rectangle(bar, (int(startX), 0), (int(endX), 50),
-                      color.astype("uint8").tolist(), -1)
-        startX = endX
-
-    # return the bar chart
-    return bar
-
-img = cv2.imread("googlesign.png")
-img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-
-img = img.reshape((img.shape[0] * img.shape[1],3)) #represent as row*column,channel number
-clt = KMeans(n_clusters=3) #cluster number
-clt.fit(img)
-
-hist = find_histogram(clt)
-bar = plot_colors2(hist, clt.cluster_centers_)
-
-plt.axis("off")
-plt.imshow(bar)
-plt.show()
+cap.release()
+cv.destroyAllWindows()
